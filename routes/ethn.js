ee0ce1780fc1b7fee0fdb46c56cfa3c68babe306
Tashrif Billah <tbillah@bwh.harvard.edu>
2021-02-16 14:47:59 -0500

    typo

--- routes/ethn.js
+++ routes/ethn.js
@@ -6,7 +6,7 @@ app2.use(express.static(path.join(__dirname, 'ethnicity-viz')))
 
 app2.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, 'ethnicity-viz', 'index.html'))
-}
+})
 
 app2.listen(8040)
 
