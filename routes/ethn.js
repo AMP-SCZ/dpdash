c1820466c18e6bcb511e18d465f5d9872b0ed62e
Tashrif Billah <tbillah@bwh.harvard.edu>
2021-02-16 11:38:28 -0500

    react js hook for ethn-viz

---
+++ routes/ethn.js
@@ -0,0 +1,14 @@
+var express= require('express')
+var path= require('path')
+var app=express()
+
+app2.use(express.static(path.join(__dirname, 'ethnicity-viz')))
+
+app2.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'ethnicity-viz', 'index.html'))
+}
+
+app2.listen(8040)
+
+module.exports=app2
+
