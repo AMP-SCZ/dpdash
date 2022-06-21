8b3a956978690b3bc841a1738a98b218d6cb7d99
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2022-06-21 08:39:24 -0500

    Updated rendering to react 18

--- views/Chart.render.react.js
+++ views/Chart.render.react.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { render } from 'react-dom'
-
+import { createRoot } from 'react-dom/client';
 import Chart from './Chart.react'
 
+const container = document.getElementById('charts');
+const root = createRoot(container);
 
-render(<Chart />, document.getElementById('charts'))
+root.render(<Chart />);
