cf2453756f8036076332e6194c13f0c6158709f1
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2022-06-23 11:07:24 -0500

    New Charts Page

--- views/Chart.react.js
+++ views/Chart.react.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
+import basePathConfig from '../server/configs/basePathConfig';
+
+const basePath = basePathConfig || '';
 
 export default function Charts() {
   return (
-    <div>
+    <>
       <h1> Charts</h1>
-    </div>
+      <br />
+      <Button
+        variant="outlined"
+        color="primary"
+        href={`${basePath}/charts/new`}
+      >
+        New Chart
+      </Button>
+    </>
   )
 }
