a2aceacc89a7cd41d2746221fc873b9ee94e9f52
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2022-06-23 09:04:11 -0500

    Template fixes

--- server/templates/Chart.template.js
+++ server/templates/Chart.template.js
@@ -4,17 +4,17 @@ const basePath = basePathConfig || ''
 
 export default () => {
   return `
-	  			<!DOCTYPE html>
-	  			<html>
-	    			<head>
-		    			<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-			  			<link rel="stylesheet" href="${basePath}/css/charts.css" />
-			  			<title>Charts - DPdash</title>
-		  			</head>
-		  			<body>
-		    			<div id="charts"></div>
-			  			<script src="${basePath}/js/chart.min.js"></script>
-		  			</body>
-	  			</html>
+			<!DOCTYPE html>
+	  	<html>
+	    	<head>
+		    	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+			  	<link rel="stylesheet" href="${basePath}/css/charts.css" />
+			  	<title>Charts - DPdash</title>
+		  	</head>
+		  	<body>
+		    	<div id="charts"></div>
+			  	<script src="${basePath}/js/chart.min.js"></script>
+		  	</body>
+	  	</html>
 	`	
 }
