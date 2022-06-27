e7887d7437d53f461cc2d10f3ae9f1531a75c184
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2022-06-27 17:24:26 -0500

    Formatting updates

--- server/routes/charts.js
+++ server/routes/charts.js
@@ -10,6 +10,7 @@ const router = Router();
 router.route('/charts')
   .get(ensureAuthenticated, async (_, res) => {
     try {
+
       return res.status(200).send(chartsListPage())
     } catch (err) {
       console.error(err.message)
@@ -21,9 +22,11 @@ router.route('/charts')
 router.route('/charts/new')
   .get(ensureAuthenticated, async (_, res) => {
     try {
+      
       return res.status(200).send(newChartPage())
     } catch (error) {
       console.error(err.message)
+
       return res.status(500).send({ message: err.message })
     }
 })
