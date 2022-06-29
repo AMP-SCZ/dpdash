2b7a6ba59fdff97adc4c7c6b42125019372af065
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2022-06-29 11:50:38 -0500

    Moved routes to constants

--- server/utils/passport/ensure-authenticated.js
+++ server/utils/passport/ensure-authenticated.js
@@ -1,6 +1,8 @@
 import basePathConfig from '../../configs/basePathConfig'
 
 const basePath = basePathConfig || ''
+const logoutForbiddenRoute = `${basePath}/logout?e=forbidden`
+const unauthorizedRoute = `${basePath}/logout?e=unauthorized`
 
 export default function ensureAuthenticated(req, res, next) {
   if (!req.isAuthenticated()) {
@@ -20,9 +22,9 @@ export default function ensureAuthenticated(req, res, next) {
       function (err, data) {
         switch(true) {
           case !data || Object.keys(data).length === 0 || !!data.blocked || err:
-            return res.redirect(`${basePath}/logout?e=forbidden`)
+            return res.redirect(logoutForbiddenRoute)
           case data.access && data.access.length === 0:
-            return res.redirect(`${basePath}/logout?e=unauthorized`)
+            return res.redirect(unauthorizedRoute)
           default:
             return next()
         }
