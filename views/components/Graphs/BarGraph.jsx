42af8342e85311b2c0343833eb4c1d64048c3922
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2022-07-07 17:06:55 -0500

    Display bar graph on screen

--- views/components/Graphs/BarGraph.jsx
+++ views/components/Graphs/BarGraph.jsx
@@ -1,25 +1,37 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from 'victory';
+import { 
+  VictoryBar, 
+  VictoryChart, 
+  VictoryAxis, 
+  VictoryTheme, 
+  VictoryStack } from 'victory';
 
 
 const BarGraph = ({ graph }) => {
-  const tickValues = Object.keys(graph.data).map((value) => ++value)
-  const tickFormat = [...new Set(graph.data.map(({siteName}) => (siteName)))]
+  const tickValues = graph.data.map((_, i) => (i))
+  const tickFormat = graph.data[0].map(({siteName}) => siteName)
+  const handlePercentage = (partialValue, totalValue) => (100 * partialValue) / totalValue
+ 
   return(
     <VictoryChart
       domainPadding={20}
+      domain={{x: [0, 6]}}
       theme={VictoryTheme.material}
     >
       <VictoryAxis
-      tickValues={tickValues}
-      tickFormat={tickFormat}
-    />
-                     <VictoryAxis
-      dependentAxis
-    />
-      <VictoryBar data={graph.data} x="siteName" y="count"
-          />
+        tickValues={tickValues}
+        tickFormat={tickFormat}
+      />
+      <VictoryAxis
+        dependentAxis
+        tickFormat={(x,_,a) =>  Math.round(handlePercentage(x, a[a.length - 1]))}
+      />
+    <VictoryStack>
+      {graph.data.map((data) => (
+        <VictoryBar data={data} x="siteName" y="count"/>
+      ))}
+    </VictoryStack>
     </VictoryChart>
     
   )
