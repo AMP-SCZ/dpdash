a2b776fb6fae70c4ee1d6163b401795d9713a9e5
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2022-07-12 15:18:18 -0500

    Format of charts router file

--- server/routes/charts.js
+++ server/routes/charts.js
@@ -164,89 +164,105 @@ router.route('/charts/:chart_id')
                 "_id" : new ObjectID(chart_id)
             }
         },
-            {
-                "$project" : {
-                    "_id" : 0.0,
-                    "assessment" : 1.0,
-                    "variable" : 1.0,
-                    "fieldLabelValueMap" : 1.0,
-                    "title" : 1.0
-                }
-            }, 
-            {
-                "$lookup" : {
-                    "from" : "toc",
-                    "localField" : "assessment",
-                    "foreignField" : "assessment",
-                    "as" : "tocList",
-                    "pipeline" : [
-                        {
-                            "$match" : {
-                                "study" : {
-                                    "$not" : {
-                                        "$eq" : "files"
-                                    },
-                                    "$in": access
-                                }
-                            }
+        {
+          $project : {
+            _id : 0.0,
+            assessment : 1.0,
+            variable : 1.0,
+            fieldLabelValueMap : 1.0,
+            title : 1.0
+          }
+        }, 
+        {
+          $lookup : {
+            from : 'toc',
+            localField : 'assessment',
+            foreignField : 'assessment',
+            as : 'tocList',
+              pipeline : [
+                {
+                  $match : {
+                    study : {
+                      $not : {
+                          $eq : 'files'
                         },
-                        {
-                            "$project" : {
-                                "collection" : 1.0,
-                                "subject" : 1.0,
-                                "study" : 1.0,
-                                "_id" : 0.0
-                            }
-                        }
-                    ]
+                      $in: access
+                    }
+                  }
+                },
+                {
+                  $project : {
+                    collection : 1.0,
+                    subject : 1.0,
+                    study : 1.0,
+                    _id : 0.0
+                  }
                 }
-            }, 
-            {
-                "$unwind" : {
-                    "path" : "$tocList"
-                }
-            }, 
-            {
-                "$unwind" : {
-                    "path" : "$fieldLabelValueMap"
-                }
+              ]
             }
-        ]
-      const result = await dataDb.collection(collections.charts).aggregate(query).toArray()
-      const individualCountsList = []
-      for await (const dcmnt of result) {
-        const { fieldLabelValueMap: { value, label }, tocList: { collection, study }, variable, title } = dcmnt
-        if(!chartTitle) chartTitle = title
-        await dataDb.collection(collection).aggregate(    [
-          {
-              "$match" : {
-                  [variable] : value
-              }
           }, 
           {
-              "$group" : {
-                  "_id" : study,
-                  count : {
-                      "$sum" : 1.0
-                  }
-              }
+            $unwind : { path : '$tocList' }
+          }, 
+          {
+            $unwind : { path : '$fieldLabelValueMap' }
           }
-      ]).forEach(({ _id, count }) => individualCountsList.push({ siteName: _id, count, fieldLabel: label }));
+      ]
+      const individualCountsList = []
+      const result = await dataDb.collection(collections.charts)
+        .aggregate(siteAndAssessmentSubjects)
+        .toArray()
+      for await (const dcmnt of result) {
+        const { 
+          fieldLabelValueMap: { 
+            value, 
+            label 
+          }, 
+          tocList: { 
+            collection, 
+            study 
+          }, 
+          variable, 
+          title 
+        } = dcmnt
+
+        if(!chartTitle) chartTitle = title
+        const subjectDocumentCount = [
+          {
+            $match : {
+              [variable] : value
+            }
+          }, 
+          {
+            $group : {
+              _id : study,
+              count : { $sum : 1.0 }
+            }
+          }
+        ]
+        await dataDb
+          .collection(collection)
+          .aggregate(subjectDocumentCount)
+          .forEach(({ _id, count }) => individualCountsList.push({ siteName: _id, count, fieldLabel: label }));
       }
-      const data = Object.values(individualCountsList.reduce(function (r, a) {
-        r[a.fieldLabel] = r[a.fieldLabel] || [];
-        r[a.fieldLabel].push(a);
-        return r;
-    }, Object.create(null))
-)
-.map((groupedCounts) => 
-Object.values(groupedCounts.reduce((acc, siteData) => {
-  acc[siteData.siteName] = acc[siteData.siteName]
-    ? { ...siteData, count: siteData.count + acc[siteData.siteName].count }
-    : siteData;
-  return acc;
-}, {}))
-)
+      
+      const data = Object
+        .values(individualCountsList
+        .reduce(function (currentSiteData, nextSiteData) {
+          currentSiteData[nextSiteData.fieldLabel] = currentSiteData[nextSiteData.fieldLabel] || [];
+          currentSiteData[nextSiteData.fieldLabel].push(nextSiteData);
+          return currentSiteData;
+        }, {}))
+        .map((groupedCounts) => 
+          Object
+          .values(groupedCounts
+            .reduce((currentSite, nextSite) => {
+              currentSite[nextSite.siteName] = currentSite[nextSite.siteName]
+              ? { ...nextSite, count: nextSite.count + currentSite[nextSite.siteName].count }
+              : nextSite;
+              return currentSite;
+            }, {}))
+        )
       const user = userFromRequest(req)
       const graph = { chart_id, data, title: chartTitle }
 
