631d8dd647cc133e4e269150f9e5bef9ed213905
Mike Stone <mikestone14@gmail.com>
2022-07-28 15:15:00 -0400

    WIP

--- server/aggregates/chartAggregates.js
+++ server/aggregates/chartAggregates.js
@@ -1,81 +1,78 @@
 import { handleNumberStringInput } from '../utils/inputHandlers'
-import { ObjectID } from 'mongodb';
+import { ObjectID } from 'mongodb'
 
 export const siteAndAssessmentSubjects = (chart_id, userAccess) => [
   {
-    $match : { _id : new ObjectID(chart_id) }
+    $match: { _id: new ObjectID(chart_id) },
   },
   {
-    $project : {
-      _id : 0.0,
-      assessment : 1.0,
-      variable : 1.0,
-      fieldLabelValueMap : 1.0,
-      title : 1.0,
-      description: 1.0
-    }
-  }, 
+    $project: {
+      _id: 0.0,
+      assessment: 1.0,
+      variable: 1.0,
+      fieldLabelValueMap: 1.0,
+      title: 1.0,
+      description: 1.0,
+    },
+  },
   {
-    $lookup : {
-      from : 'toc',
-      localField : 'assessment',
-      foreignField : 'assessment',
-      as : 'tocList',
-        pipeline : [
-          {
-            $match : {
-              study : {
-                $not : {
-                    $eq : 'files'
-                  },
-                $in: userAccess
-              }
-            }
+    $lookup: {
+      from: 'toc',
+      localField: 'assessment',
+      foreignField: 'assessment',
+      as: 'tocList',
+      pipeline: [
+        {
+          $match: {
+            study: {
+              $not: {
+                $eq: 'files',
+              },
+              $in: userAccess,
+            },
           },
-          {
-            $project : {
-              collection : 1.0,
-              subject : 1.0,
-              study : 1.0,
-              _id : 0.0
-            }
-          }
-        ]
-      }
-    }, 
-    {
-      $unwind : { path : '$tocList' }
-    }, 
-    {
-      $unwind : { path : '$fieldLabelValueMap' }
-    }
- ]
+        },
+        {
+          $project: {
+            collection: 1.0,
+            subject: 1.0,
+            study: 1.0,
+            _id: 0.0,
+          },
+        },
+      ],
+    },
+  },
+  {
+    $unwind: { path: '$tocList' },
+  },
+]
 
 export const subjectDocumentCount = (variable, value, study) => [
   {
-    $match : {
-      [variable] : handleNumberStringInput(value)
-    }
-  }, 
+    $match: {
+      [variable]: handleNumberStringInput(value),
+    },
+  },
   {
-    $group : {
-      _id : study,
-      count : { $sum : 1.0 }
-    }
-  }
+    $group: {
+      _id: study,
+      count: { $sum: 1.0 },
+    },
+  },
 ]
 
 export const legendQuery = (chart_id) => [
   {
-    $match : { _id : new ObjectID(chart_id) }
+    $match: { _id: new ObjectID(chart_id) },
   },
   {
-    $project : {
-      _id : 0.0,
-      fieldLabelValueMap : 1.0,
-    }
+    $project: {
+      _id: 0.0,
+      fieldLabelValueMap: 1.0,
+    },
   },
   {
-    $unwind: { path: '$fieldLabelValueMap' }
-  }
+    $unwind: { path: '$fieldLabelValueMap' },
+  },
 ]
--- server/controllers/chartsController.js
+++ server/controllers/chartsController.js
@@ -1,50 +1,74 @@
 import { collections } from '../utils/mongoCollections'
 import {
   siteAndAssessmentSubjects,
-  subjectDocumentCount,
   legendQuery,
 } from '../aggregates/chartAggregates'
 
 export const graphDataController = async (dataDb, userAccess, chart_id) => {
-  let chartTitle;
-  let chartDescription;
-  const individualCountsList = []
-  const sitesAndAssessments = await dataDb
+  // Get all the assessment's subjects for each site
+  const chart = await dataDb
+    .collection(collections.charts)
+    .findOne({ _id: ObjectId(chart_id) })
+  const allSubjects = await dataDb
     .collection(collections.charts)
     .aggregate(siteAndAssessmentSubjects(chart_id, userAccess))
     .toArray()
 
-  for await (const dcmnt of sitesAndAssessments) {
-    const { 
-      fieldLabelValueMap: { 
-        value, 
-        label 
-      }, 
-      tocList: { 
-        collection, 
-        study 
-      }, 
-      variable, 
-      title,
-      description
-    } = dcmnt
-    chartTitle ??= title
-    chartDescription ??= description
+  const allCounts = {}
+  chart.fieldLabelValueMap.forEach((fieldLabelValueMap) => {
+    allCounts[fieldLabelValueMap.label] = {}
+  })
+  // For each site's subjects
+  for await (const subject of allSubjects) {
+    const { study } = subject // site and study are the same thing
+    allCounts[fieldLabelValueMap.label][study] =
+      allCounts[fieldLabelValueMap.label][study] || 0
 
-    await dataDb
-      .collection(collection)
-      .aggregate(subjectDocumentCount(variable, value, study))
-      .forEach(({ _id, count }) => individualCountsList.push({ siteName: _id, count, fieldLabel: label }));
+    // For each value/label pair
+    chart.fieldLabelValueMap.forEach((fieldLabelValueMap) => {
+      const hasValue = subject[chart.variable] === fieldLabelValueMap.value
+
+      if (hasValue) {
+        allCounts[fieldLabelValueMap.label][study] += 1
+      }
+    })
   }
 
-  return { 
-    chartTitle, 
-    chartDescription, 
-    individualCountsList 
+  return {
+    chart,
+    allCounts,
   }
 }
 
-export const fieldValuesController = async(dataDb, chart_id) => await dataDb
-  .collection(collections.charts)
-  .aggregate(legendQuery(chart_id))
-  .toArray()
+// Front-end logic
+// const labels = Object.keys(allCounts)
+// labels.map((label) => {
+//   Object.keys(allCounts[label]).map((site) => {
+//     return { x: site, y: allCounts[label][site] }
+//   })
+// })
+
+// allCounts
+// {
+// black: {
+// yale: 0,
+// usc: 1,
+// },
+// white: {
+// yale: 1,
+// usc: 10,
+// },
+// hispanic: {
+// yale: 11,
+// usc: 12,
+// },
+// [label]: {
+// [site]: [count]
+// }
+// }
+
+export const fieldValuesController = async (dataDb, chart_id) =>
+  await dataDb
+    .collection(collections.charts)
+    .aggregate(legendQuery(chart_id))
+    .toArray()
