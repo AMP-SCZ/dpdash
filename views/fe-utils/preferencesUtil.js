ac406194c712160522e6718ec0e8a81a59939cd0
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2022-08-12 09:44:35 -0500

    Preference function
    * Used a default object in the babyProofPreference function

--- views/fe-utils/preferencesUtil.js
+++ views/fe-utils/preferencesUtil.js
@@ -1,11 +1,11 @@
-export const babyProofPreferences = (preferences) => {
-  let preference = {}
-  preference['star'] = 'star' in preferences ? preferences['star'] : {}
-  preference['sort'] = 'sort' in preferences ? preferences['sort'] : 0
-  preference['config'] = 'config' in preferences ? preferences['config'] : ''
-  preference['complete'] =
-    'complete' in preferences ? preferences['complete'] : {}
-  return preference
+const babyProofPreferences = (preferences) => {
+  const defaultPreferences = {
+    complete: {},
+    star: {},
+    sort: 0,
+  }
+
+  return { ...defaultPreferences, ...preferences }
 }
 
 export const preparePreferences = (configuration, preferences) => {
