9c2ee658fa1d23719239cab98d359a8b7e8bf7d5
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2022-11-14 17:00:01 -0500

    async await ensure authenticated
    * Added async await syntax to ensure authenticated user

--- server/utils/passport/ensure-authenticated.js
+++ server/utils/passport/ensure-authenticated.js
@@ -4,28 +4,26 @@ const basePath = basePathConfig || ''
 const logoutForbiddenRoute = `${basePath}/logout?e=forbidden`
 const unauthorizedRoute = `${basePath}/logout?e=unauthorized`
 
-export default function ensureAuthenticated(req, res, next) {
+export default async function ensureAuthenticated(req, res, next) {
   if (!req.isAuthenticated()) {
     return res.redirect(`${basePath}/logout`)
   }
   const { appDb } = req.app.locals
-  appDb.users
-    .findUniqueOrThrow({
-      where: { uid: req.user },
-      select: {
-        access: true,
-        blocked: true,
-        role: true,
-      },
-    })
-    .then((data) => {
-      switch (true) {
-        case !data || Object.keys(data).length === 0 || !!data.blocked:
-          return res.redirect(logoutForbiddenRoute)
-        case data.access && data.access.length === 0:
-          return res.redirect(unauthorizedRoute)
-        default:
-          return next()
-      }
-    })
+  const user = await appDb.users.findUniqueOrThrow({
+    where: { uid: req.user },
+    select: {
+      access: true,
+      blocked: true,
+      role: true,
+    },
+  })
+
+  switch (true) {
+    case !user || Object.keys(user).length === 0 || !!user.blocked:
+      return res.redirect(logoutForbiddenRoute)
+    case user.access && user.access.length === 0:
+      return res.redirect(unauthorizedRoute)
+    default:
+      return next()
+  }
 }
