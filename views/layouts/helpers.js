11d37c682878323ab31a46176d5b2c7f9e3bd6ac
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2023-06-22 13:49:44 -0500

    Route constants
    * Added route constantsi

--- views/layouts/helpers.js
+++ views/layouts/helpers.js
@@ -1,6 +1,7 @@
+import { routes } from '../routes/routes'
 export const headerTitle = (pathname) => {
   switch (true) {
-    case pathname === '/configs':
+    case pathname === routes.configs:
       return 'Configurations'
     default:
       return 'DpDash'
