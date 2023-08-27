d24a9c566ff06444dd8ee34400923dc4097a9f00
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2023-08-27 13:17:41 -0500

    Assessment day data
    * Added routes to assessment day that will intake import
    * Added logic to handle imports
    
    Metdata upload
    
    * Metadata upload from server

---
+++ server/controllers/siteMetadataController/index.js
@@ -0,0 +1,20 @@
+import SiteMetadataModel from '../../models/SiteMetadataModel'
+import _ from 'lodash'
+
+const SiteMetadataController = {
+  create: async (req, res) => {
+    try {
+      const { appDb } = req.app.locals
+      const { metadata, participants } = req.body
+      const { study } = metadata
+
+      await SiteMetadataModel.upsert(appDb, { study }, metadata, participants)
+
+      return res.status(200).json({ data: 'Metadata imported successfully.' })
+    } catch (error) {
+      return res.status(401).json({ message: error.message })
+    }
+  },
+}
+
+export default SiteMetadataController
