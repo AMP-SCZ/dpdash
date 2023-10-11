d95c571be06267f0f7e05e8433d13db1177af14c
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2023-10-11 15:43:43 -0500

    Modify FilterModel
    * Modified Filter Model

--- server/models/FiltersModel/index.js
+++ server/models/FiltersModel/index.js
@@ -1,38 +1,41 @@
 import { STUDIES_TO_OMIT, TRUE_STRING, FALSE_STRING } from '../../constants'
 
+const DEFAULT_FILTERS = {
+  chrcrit_part: [
+    { name: 'HC', value: TRUE_STRING },
+    { name: 'CHR', value: TRUE_STRING },
+    { name: 'Missing', value: TRUE_STRING },
+  ],
+  included_excluded: [
+    { name: 'Included', value: TRUE_STRING },
+    { name: 'Excluded', value: FALSE_STRING },
+    { name: 'Missing', value: FALSE_STRING },
+  ],
+  sex_at_birth: [
+    { name: 'Male', value: TRUE_STRING },
+    { name: 'Female', value: TRUE_STRING },
+    { name: 'Missing', value: TRUE_STRING },
+  ],
+  sites: [],
+}
+
 const FiltersModel = {
   calculateFilters: function (filters, userAccess) {
     if (!filters) {
-      this.defaultFilters.sites = userAccess
-        .filter((site) => !STUDIES_TO_OMIT.includes(site))
-        .sort((prevSite, nextSite) => (prevSite < nextSite ? -1 : 1))
-
-      return this.defaultFilters
+      return {
+        ...DEFAULT_FILTERS,
+        sites: userAccess
+          .filter((site) => !STUDIES_TO_OMIT.includes(site))
+          .sort((prevSite, nextSite) => (prevSite < nextSite ? -1 : 1)),
+      }
     } else {
-      filters.sites = Object.values(filters.sites)
-        .map(({ value }) => value)
-        .sort((prevSite, nextSite) => (prevSite < nextSite ? -1 : 1))
-
-      return filters
+      return {
+        ...filters,
+        sites: Object.values(filters.sites)
+          .map(({ value }) => value)
+          .sort((prevSite, nextSite) => (prevSite < nextSite ? -1 : 1)),
+      }
     }
-  },
-  defaultFilters: {
-    chrcrit_part: [
-      { name: 'HC', value: TRUE_STRING },
-      { name: 'CHR', value: TRUE_STRING },
-      { name: 'Missing', value: TRUE_STRING },
-    ],
-    included_excluded: [
-      { name: 'Included', value: TRUE_STRING },
-      { name: 'Excluded', value: FALSE_STRING },
-      { name: 'Missing', value: FALSE_STRING },
-    ],
-    sex_at_birth: [
-      { name: 'Male', value: TRUE_STRING },
-      { name: 'Female', value: TRUE_STRING },
-      { name: 'Missing', value: TRUE_STRING },
-    ],
-    sites: [],
   },
   normalizeFilters: (filters) => {
     return {
