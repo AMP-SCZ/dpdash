01971f601a8f8b3fb8a11d2034e7c5f226c39c6c
Jack Curtis <jack@gnar.dog>
2023-10-20 08:42:26 -0400

    add namespacing for secrets

--- bin/echo-secrets.sh
+++ bin/echo-secrets.sh
@@ -7,22 +7,22 @@ read -p "This script will log secret values to the console. Are you sure you wis
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   aws ssm get-parameter \
-      --name "DPDASH_MONGODB_ADMIN_USER" \
+      --name "DPDASH_MONGODB_ADMIN_USER_DEV" \
       --with-decryption
 
   aws ssm get-parameter \
-      --name "DPDASH_MONGODB_ADMIN_PASSWORD" \
+      --name "DPDASH_MONGODB_ADMIN_PASSWORD_DEV" \
       --with-decryption
 
   aws ssm get-parameter \
-      --name "DPDASH_IMPORT_API_USERS" \
+      --name "DPDASH_IMPORT_API_USERS_DEV" \
       --with-decryption
 
   aws ssm get-parameter \
-      --name "DPDASH_IMPORT_API_KEYS" \
+      --name "DPDASH_IMPORT_API_KEYS_DEV" \
       --with-decryption
 
   aws ssm get-parameter \
-      --name "DPDASH_SESSION_SECRET" \
+      --name "DPDASH_SESSION_SECRET_DEV" \
       --with-decryption
 fi
--- bin/generate-secrets.sh
+++ bin/generate-secrets.sh
@@ -6,26 +6,26 @@
 # Set an environment variable as a comma separated list of usernames for the API users named DPDASH_IMPORT_API_USERS
 
 aws ssm put-parameter \
-    --name "DPDASH_MONGODB_ADMIN_USER" \
-    --value $DPDASH_MONGODB_ADMIN_USER \
+    --name "DPDASH_MONGODB_ADMIN_USER_DEV" \
+    --value $DPDASH_MONGODB_ADMIN_USER_DEV \
     --type SecureString \
     --tags "Key=Name,Value=dpdash"
 
 aws ssm put-parameter \
-    --name "DPDASH_MONGODB_ADMIN_PASSWORD" \
+    --name "DPDASH_MONGODB_ADMIN_PASSWORD_DEV" \
     --value $(openssl rand -base64 32 | tr -d "+=/") \
     --type SecureString \
     --tags "Key=Name,Value=dpdash"
 
 aws ssm put-parameter \
-    --name "DPDASH_IMPORT_API_USERS" \
-    --value $DPDASH_IMPORT_API_USERS \
+    --name "DPDASH_IMPORT_API_USERS_DEV" \
+    --value $DPDASH_IMPORT_API_USERS_DEV \
     --type SecureString \
     --tags "Key=Name,Value=dpdash"
 
-export DPDASH_NUMBER_OF_USERS=$(($(echo $DPDASH_IMPORT_API_USERS | tr -cd , | wc -c) + 1))
+export DPDASH_NUMBER_OF_USERS=$(($(echo $DPDASH_IMPORT_API_USERS_DEV | tr -cd , | wc -c) + 1))
 aws ssm put-parameter \
-    --name "DPDASH_IMPORT_API_KEYS" \
+    --name "DPDASH_IMPORT_API_KEYS_DEV" \
     --value $(
       for i in seq $DPDASH_NUMBER_OF_USERS; do
         echo $(openssl rand -base64 32 | tr -d "+=/")
@@ -35,7 +35,7 @@ aws ssm put-parameter \
     --tags "Key=Name,Value=dpdash"
 
 aws ssm put-parameter \
-    --name "DPDASH_SESSION_SECRET" \
+    --name "DPDASH_SESSION_SECRET_DEV" \
     --value $(openssl rand -base64 32 | tr -d "+=/") \
     --type SecureString \
     --tags "Key=Name,Value=dpdash"
