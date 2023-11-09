973b7a1e363bdfeefd37c193926de54660a69844
Mike Stone <mikestone14@gmail.com>
2023-11-09 09:20:49 -0500

    update the user profile form

--- views/forms/UserProfileForm/index.jsx
+++ views/forms/UserProfileForm/index.jsx
@@ -1,86 +1,116 @@
-import { Button, Tooltip } from '@mui/material'
-import { Controller } from 'react-hook-form'
+import React, { useRef } from 'react'
+import { Button } from '@mui/material'
+import { Controller, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
 import { EMAIL_REGEX } from '../../../constants'
 import TextInput from '../TextInput'
 
-const UserProfileForm = ({ control, onSubmit, setUser, user }) => {
+const schema = yup.object({
+  company: yup.string(),
+  department: yup.string(),
+  display_name: yup.string().required(),
+  icon: yup.string(),
+  mail: yup.string().email().required(),
+  title: yup.string(),
+})
+
+const UserProfileForm = ({ initialValues, onSubmit }) => {
+  const profileImageRef = useRef()
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    watch,
+  } = useForm({
+    defaultValues: initialValues,
+    resolver: yupResolver(schema),
+  })
+
   return (
-    <form onSubmit={onSubmit}>
-      <Controller
-        control={control}
-        name="icon"
-        render={({ field: { value, onChange, ...field } }) => {
-          return (
-            <div>
-              <input
-                accept="image/*"
-                id="icon"
-                multiple
-                type="file"
-                {...field}
-                value={value?.fileName}
-                onChange={(event) => {
-                  const { files } = event.target
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name="icon"
+          render={({ field }) => {
+            return (
+              <div>
+                <input
+                  accept="image/*"
+                  data-testid="icon-input"
+                  id="icon"
+                  multiple
+                  type="file"
+                  {...field}
+                  value={field.value?.fileName}
+                  onChange={(event) => {
+                    const { files } = event.target
 
-                  if (files[0]) {
-                    const reader = new FileReader()
+                    if (files[0]) {
+                      const reader = new FileReader()
 
-                    reader.readAsDataURL(files[0])
-                    reader.onload = (e) => {
-                      setUser({ ...user, icon: e.target.result })
-                      onChange(e.target.result)
+                      reader.readAsDataURL(files[0])
+                      reader.onload = (e) => {
+                        field.onChange(e.target.result)
+                      }
                     }
-                  }
-                }}
-              />
-              <label htmlFor="icon">
-                <span>
-                  <Tooltip title="Edit Profile Photo"></Tooltip>
-                </span>
-              </label>
-            </div>
-          )
-        }}
+                  }}
+                />
+                <label htmlFor="icon">Edit Profile Photo</label>
+              </div>
+            )
+          }}
+        />
+        <TextInput
+          control={control}
+          errors={errors.display_name}
+          label="Full Name"
+          name="display_name"
+          fullWidth={true}
+        />
+        <TextInput
+          control={control}
+          errors={errors.mail}
+          label="Email"
+          pattern={EMAIL_REGEX}
+          name="mail"
+          fullWidth={true}
+        />
+        <TextInput
+          control={control}
+          errors={errors.title}
+          label="Title"
+          name="title"
+          fullWidth={true}
+        />
+        <TextInput
+          control={control}
+          errors={errors.department}
+          label="Department"
+          name="department"
+          fullWidth={true}
+        />
+        <TextInput
+          control={control}
+          errors={errors.company}
+          label="Company"
+          name="company"
+          fullWidth={true}
+        />
+        <div>
+          <Button variant="outlined" type="submit">
+            Save
+          </Button>
+        </div>
+      </form>
+      <img
+        ref={profileImageRef}
+        src={watch('icon')}
+        style={{ height: 200, width: 200, objectFit: 'scale-down' }}
       />
-      <TextInput
-        control={control}
-        label="Full Name"
-        name="display_name"
-        fullWidth={true}
-      />
-      <TextInput
-        control={control}
-        label="Email"
-        type="email"
-        pattern={EMAIL_REGEX}
-        name="mail"
-        fullWidth={true}
-      />
-      <TextInput
-        control={control}
-        label="Title"
-        name="title"
-        fullWidth={true}
-      />
-      <TextInput
-        control={control}
-        label="Department"
-        name="department"
-        fullWidth={true}
-      />
-      <TextInput
-        control={control}
-        label="Company"
-        name="company"
-        fullWidth={true}
-      />
-      <div>
-        <Button variant="outlined" type="submit">
-          Save
-        </Button>
-      </div>
-    </form>
+    </>
   )
 }
 
--- views/pages/AccountPage.jsx
+++ views/pages/AccountPage.jsx
@@ -1,20 +1,14 @@
-import React, { useRef } from 'react'
-import { useForm } from 'react-hook-form'
+import React from 'react'
 import { useOutletContext } from 'react-router-dom'
 
 import api from '../api'
 import UserProfileForm from '../forms/UserProfileForm'
 
 const AccountPage = () => {
-  const canvasRef = useRef()
-  const profileImageRef = useRef()
   const { user, setUser, setNotification } = useOutletContext()
   const { icon, display_name, mail, title, department, company } = user
-  const { handleSubmit, control, watch } = useForm({
-    defaultValues: { icon, display_name, mail, title, department, company },
-  })
 
-  const handleFormSubmit = async (userProfileValues) => {
+  const onSubmit = async (userProfileValues) => {
     try {
       const updatedUser = await api.users.update(user.uid, {
         ...user,
@@ -34,42 +28,11 @@ const AccountPage = () => {
     }
   }
 
-  const scaleDownImage = () => {
-    const image = profileImageRef.current
-    const canvas = canvasRef.current
-    const ctx = canvas.getContext('2d')
-    const x = 0
-    const y = 0
-    let sx = 0
-    let sy = (image.naturalHeight - image.naturalWidth) / 2
-    let swidth = image.naturalWidth
-    let sheight = image.naturalWidth
-
-    if (image.naturalHeight < image.naturalWidth) {
-      sy = 0
-      sx = (image.naturalWidth - image.naturalHeight) / 2
-      swidth = image.naturalHeight
-      sheight = image.naturalHeight
-    }
-
-    canvas.height = 200
-    canvas.width = 200
-
-    ctx.drawImage(image, sx, sy, swidth, sheight, x, y, 200, 200)
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-  }
-
   return (
-    <div>
-      <UserProfileForm
-        control={control}
-        onSubmit={handleSubmit(handleFormSubmit)}
-        user={user}
-        setUser={setUser}
-      />
-      <img ref={profileImageRef} onLoad={scaleDownImage} src={watch('icon')} />
-      <canvas ref={canvasRef} />
-    </div>
+    <UserProfileForm
+      initialValues={{ icon, display_name, mail, title, department, company }}
+      onSubmit={onSubmit}
+    />
   )
 }
 
