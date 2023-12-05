58f2a8956a0fe244bbd450591e1c9bcbcff9bceb
Jack Curtis <jack@gnar.dog>
2023-12-05 09:23:51 -0500

    fix input order

--- views/forms/AdminUsersSearchForm/index.jsx
+++ views/forms/AdminUsersSearchForm/index.jsx
@@ -1,11 +1,11 @@
-import { yupResolver } from '@hookform/resolvers/yup'
 import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
-import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 import SearchIcon from '@mui/icons-material/Search'
+import { InputAdornment } from '@mui/material'
+import * as yup from 'yup'
 
 import ControlledMultiSelect from '../ControlledMultiSelect'
-import { InputAdornment } from '@mui/material'
 
 const schema = yup.object({
   users: yup.array()
