cbcf8d124f1d2fe6743a7a9f5b7d5a5aac4ef93e
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2024-01-17 16:03:34 -0600

    Handle loading default config
    * default config

--- views/forms/ConfigFields/index.jsx
+++ views/forms/ConfigFields/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Grid } from '@mui/material'
 import ConfigAssessmentFormFields from '../ConfigAssessmentFormFields'
 import ConfigTypeFormFields from '../ConfigTypeFormFields'
 
@@ -14,25 +13,30 @@ const ConfigFormFields = ({
   return (
     <React.Fragment>
       <ConfigTypeFormFields control={control} friendsList={friendsList} />
-      <Grid container columnSpacing={1} rowSpacing={3}>
+      <div
+        style={{
+          display: 'flex',
+          gap: '10px',
+          flexFlow: 'wrap',
+        }}
+      >
         {fields.map((field, index) => {
           const { id, ...rest } = field
 
           return (
-            <Grid item xs={12} md={5} lg={4} key={id}>
-              <ConfigAssessmentFormFields
-                colors={colors}
-                control={control}
-                index={index}
-                id={id}
-                onCopy={onCopy}
-                onRemove={onRemove}
-                {...rest}
-              />
-            </Grid>
+            <ConfigAssessmentFormFields
+              colors={colors}
+              control={control}
+              index={index}
+              id={id}
+              key={id}
+              onCopy={onCopy}
+              onRemove={onRemove}
+              {...rest}
+            />
           )
         })}
-      </Grid>
+      </div>
     </React.Fragment>
   )
 }
