be222bfa7c8266e1f4d0badc25b9960a35369821
Jack Curtis <jack@gnar.dog>
2024-01-17 13:20:17 -0500

    rebase and fix conflicts

--- server/utils/passport/ensure-authenticated.js
+++ server/utils/passport/ensure-authenticated.js
@@ -3,31 +3,27 @@ import { isAccountExpired } from './helpers'
 import { logout } from './logout'
 
 export default async function ensureAuthenticated(req, res, next) {
-  try {
-    if (!req.isAuthenticated())
-      return res.status(401).json({ error: 'Please login' })
+  if (!req.isAuthenticated())
+    return res.status(401).json({ error: 'Please login' })
 
-    const { appDb } = req.app.locals
-    const uid = req.user
-    const user = await UserModel.findOne(appDb, { uid })
-    const { account_expires, blocked, access, role } = user
+  const { appDb } = req.app.locals
+  req.user = await UserModel.findOne(appDb, { uid: req.user.uid })
 
-    switch (true) {
-      case isAccountExpired(account_expires, role):
-        logout(req, res, next)
+  const { account_expires, blocked, access, role } = req.user
 
-        return res.status(401).json({ error: 'Account is expired' })
+  switch (true) {
+    case isAccountExpired(account_expires, role):
+      logout(req, res, next)
 
-      case !!blocked:
-        return res
-          .status(403)
-          .json({ error: 'Account is blocked, please contact your admin' })
-      case access.length === 0:
-        return res.status(403).json({ error: 'Forbidden.' })
-      default:
-        return next()
-    }
-  } catch (error) {
-    return res.status(500).json({ error: error.message })
+      return res.status(401).json({ error: 'Account is expired' })
+
+    case !!blocked:
+      return res
+        .status(403)
+        .json({ error: 'Account is blocked, please contact your admin' })
+    case access.length === 0:
+      return res.status(403).json({ error: 'Forbidden.' })
+    default:
+      return next()
   }
 }
