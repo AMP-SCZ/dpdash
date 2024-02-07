aeb237abc35eacd17c1c3bade21e5469ec3c379c
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2024-02-07 09:10:50 -0600

    Pointing
    * Start pointing to single db

--- server/controllers/chartsController/index.js
+++ server/controllers/chartsController/index.js
@@ -56,6 +56,7 @@ const chartsController = {
 
       return res.status(200).json({ data: chartList })
     } catch (error) {
+      console.log(error)
       return res.status(400).json({ error: error.message })
     }
   },
