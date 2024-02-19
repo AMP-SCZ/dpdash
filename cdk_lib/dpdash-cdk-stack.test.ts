2900907f107bdf9168d9cebcbd71a7f288b66881
Jack Curtis <jack@gnar.dog>
2024-02-19 08:26:52 -0500

    move template factory to a fixture file

--- cdk_lib/dpdash-cdk-stack.test.ts
+++ cdk_lib/dpdash-cdk-stack.test.ts
@@ -1,6 +1,5 @@
-import { Capture, Match, Template } from 'aws-cdk-lib/assertions';
-import * as cdk from 'aws-cdk-lib';
-import { DpdashCdkStack } from './dpdash-cdk-stack';
+import { createTemplate } from './test/fixtures'
+import { DpdashCdkStack } from './dpdash-cdk-stack'
 
 describe('DPDashCDKStack', () => {
   const OLD_ENV = process.env
@@ -18,12 +17,6 @@ describe('DPDashCDKStack', () => {
     }
   }
 
-  const createTemplate = () => {
-    const app = new cdk.App()
-    const dpDashCdkStack = new DpdashCdkStack(app, "DPDashCDKStack")
-    return Template.fromStack(dpDashCdkStack)
-  }
-
   beforeEach(() => {
     jest.resetModules()
     process.env = { ...OLD_ENV };
@@ -35,14 +28,14 @@ describe('DPDashCDKStack', () => {
 
   it('creates a VPC', () => {
     setEnv()
-    const template = createTemplate()
+    const template = createTemplate(DpdashCdkStack)
 
     template.hasResource('AWS::EC2::VPC', {})
   })
 
   it('creates a DocumentDB Cluster', () => {
     setEnv()
-    const template = createTemplate()
+    const template = createTemplate(DpdashCdkStack)
 
     template.hasResource('AWS::DocDB::DBCluster', {})
     template.hasResource('AWS::DocDB::DBInstance', {})
@@ -50,7 +43,7 @@ describe('DPDashCDKStack', () => {
 
   it('creates an Application Load Balanced Fargate Service', () => {
     setEnv()
-    const template = createTemplate()
+    const template = createTemplate(DpdashCdkStack)
 
     template.hasResource('AWS::ElasticLoadBalancingV2::LoadBalancer', {})
     template.hasResource('AWS::ECS::Cluster', {
@@ -73,25 +66,25 @@ describe('DPDashCDKStack', () => {
   describe('when the DPDASH_DEV flag is set to "1"', () => {
     it('creates a Hosted Zone', () => {
       setEnv()
-      const template = createTemplate()
+      const template = createTemplate(DpdashCdkStack)
       template.hasResource('AWS::Route53::HostedZone', {})
     })
 
     it('creates a Certificate', () => {
       setEnv()
-      const template = createTemplate()
+      const template = createTemplate(DpdashCdkStack)
       template.hasResource('AWS::CertificateManager::Certificate', {})
     })
 
     it('creates an SES Email Identity', () => {
       setEnv()
-      const template = createTemplate()
+      const template = createTemplate(DpdashCdkStack)
       template.hasResource('AWS::SES::EmailIdentity', {})
     })
 
     it('creates a public Application Load Balanced Fargate Service', () => {
       setEnv()
-      const template = createTemplate()
+      const template = createTemplate(DpdashCdkStack)
 
       template.hasResource('AWS::ElasticLoadBalancingV2::LoadBalancer', {
         'Properties': {
@@ -103,12 +96,12 @@ describe('DPDashCDKStack', () => {
   describe('when the DPDASH_DEV flag is not set to "1"', () => {
     it('throws an error if the CERT_ARN and SES_IDENTITY_ARN are missing', () => {
       setEnv({ DPDASH_DEV: undefined })
-      expect(createTemplate).toThrowError("Missing required environment variables: CERT_ARN, SES_IDENTITY_ARN")
+      expect(() => createTemplate(DpdashCdkStack)).toThrowError("Missing required environment variables: CERT_ARN, SES_IDENTITY_ARN")
     })
 
     it('creates a private Application Load Balanced Fargate Service', () => {
       setEnv({ DPDASH_DEV: undefined, CERT_ARN: 'foo', SES_IDENTITY_ARN: 'bar' })
-      const template = createTemplate()
+      const template = createTemplate(DpdashCdkStack)
 
       template.hasResource('AWS::ElasticLoadBalancingV2::LoadBalancer', {
           'Properties': {
