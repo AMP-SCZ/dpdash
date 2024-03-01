bd63cc25fb9867dccd3a5e5f5b6523c477b639cb
Jack Curtis <jack@gnar.dog>
2024-03-01 10:09:10 -0500

    remove dependency on facets

--- server/services/FiltersService/index.js
+++ server/services/FiltersService/index.js
@@ -6,7 +6,7 @@ import {
 } from '../../constants'
 import StudiesModel from '../../models/StudiesModel'
 
-const FILTER_TO_MONGO_VALUE_MAP = {
+export const FILTER_TO_MONGO_VALUE_MAP = {
   HC: 2,
   CHR: 1,
   Missing: null,
@@ -66,149 +66,40 @@ class FiltersService {
     }
   }
 
-  barChartMongoQueries = () => {
-    const filters = this.filters
-    const $facet = {}
-    const chrCritFilters = filters.chrcrit_part
-      .filter((f) => f.value === TRUE_STRING)
-      .map((filter) => FILTER_TO_MONGO_VALUE_MAP[filter.name])
-    const includedExcludedFilters = filters.included_excluded
-      .filter((f) => f.value === TRUE_STRING)
-      .map((filter) => FILTER_TO_MONGO_VALUE_MAP[filter.name])
-    const sexAtBirthFilters = filters.sex_at_birth
-      .filter((f) => f.value === TRUE_STRING)
-      .map((filter) => FILTER_TO_MONGO_VALUE_MAP[filter.name])
-
-    const isIncOrExcOnly =
-      !chrCritFilters.length && includedExcludedFilters.length
-    const isCritOnly = chrCritFilters.length && !includedExcludedFilters.length
-    const isIncAndCrit = chrCritFilters.length && includedExcludedFilters.length
-
-    if (isIncAndCrit) {
-      $facet.formInclusion = [
-        {
-          $match: {
-            assessment: INCLUSION_EXCLUSION_CRITERIA_FORM,
+  get filterQueries() {
+    const filterQueries = []
+    for (let filterKey of Object.keys(this.filters)) {
+      const filter = this.filters[filterKey]
+      const includedFilters = filter.filter(({_, value}) => value === TRUE_STRING)
+      const includedValues = includedFilters.map(({name, _}) => FILTER_TO_MONGO_VALUE_MAP[name])
+      switch (filterKey) {
+        case 'included_excluded':
+          filterQueries.push({
             $and: [
-              {
-                dayData: {
-                  $elemMatch: { chrcrit_part: { $in: chrCritFilters } },
-                },
-              },
-              {
-                dayData: {
-                  $elemMatch: {
-                    included_excluded: { $in: includedExcludedFilters },
-                  },
-                },
-              },
-            ],
-            study: { $in: this.filters.sites },
-          },
-        },
-      ]
+              {assessment: INCLUSION_EXCLUSION_CRITERIA_FORM},
+              {'dayData.included_excluded': {$in: includedValues } }
+            ]
+          })
+          break
+        case 'chrcrit_part':
+          filterQueries.push({
+            $and: [
+              {assessment: INCLUSION_EXCLUSION_CRITERIA_FORM},
+              {'dayData.chrcrit_part': {$in: includedValues } }
+            ]
+          })
+          break
+        case 'sex_at_birth':
+          filterQueries.push({
+            $and: [
+              {assessment: SOCIODEMOGRAPHICS_FORM},
+              {'dayData.chrdemo_sexassigned': {$in: includedValues } }
+            ]
+          })
+          break
+      }
     }
-    if (isIncOrExcOnly || isCritOnly) {
-      const variable = chrCritFilters.length
-        ? 'chrcrit_part'
-        : 'included_excluded'
-      const values = chrCritFilters.length
-        ? chrCritFilters
-        : includedExcludedFilters
-
-      $facet.formInclusion = [
-        {
-          $match: {
-            assessment: INCLUSION_EXCLUSION_CRITERIA_FORM,
-            study: {
-              $in: this.filters.sites,
-            },
-            dayData: {
-              $elemMatch: { [variable]: { $in: values } },
-            },
-          },
-        },
-      ]
-    }
-    if (sexAtBirthFilters.length) {
-      $facet.sexAtBirth = [
-        {
-          $match: {
-            assessment: SOCIODEMOGRAPHICS_FORM,
-            dayData: {
-              $elemMatch: {
-                chrdemo_sexassigned: { $in: sexAtBirthFilters },
-              },
-            },
-          },
-        },
-      ]
-    }
-    return Object.keys($facet).length > 1
-      ? [
-          { $facet },
-          {
-            $project: {
-              participants: {
-                $setIntersection: [
-                  '$formInclusion.participant',
-                  '$sexAtBirth.participant',
-                ],
-              },
-            },
-          },
-        ]
-      : $facet
-  }
-
-  _buildFacetForFilters = ({
-    isSexAtBirthFilterActive,
-    isInclusionCriteriaFilterActive,
-    sites,
-  }) => {
-    const facetForFilters = {}
-    if (isSexAtBirthFilterActive) {
-      facetForFilters.socioDemographics = [
-        {
-          $match: {
-            assessment: SOCIODEMOGRAPHICS_FORM,
-            study: { $in: sites },
-          },
-        },
-        {
-          $project: {
-            ...INDIVIDUAL_FILTERS_MONGO_PROJECTION,
-          },
-        },
-        {
-          $addFields: {
-            filter: SOCIODEMOGRAPHICS_FORM,
-          },
-        },
-      ]
-    }
-    if (isInclusionCriteriaFilterActive) {
-      facetForFilters.inclusionCriteria = [
-        {
-          $match: {
-            assessment: INCLUSION_EXCLUSION_CRITERIA_FORM,
-            study: { $in: sites },
-          },
-        },
-        {
-          $project: {
-            ...INDIVIDUAL_FILTERS_MONGO_PROJECTION,
-          },
-        },
-        {
-          $addFields: {
-            filter: INCLUSION_EXCLUSION_CRITERIA_FORM,
-          },
-        },
-      ]
-    }
-
-    return facetForFilters
+    return filterQueries
   }
 }
 
