08f12d1fb61095e2fc670cd7db57c80526e2fcfc
Jack Curtis <jack@gnar.dog>
2024-03-28 13:18:11 -0400

    fix indentation

--- views/forms/ChartShareForm/index.jsx
+++ views/forms/ChartShareForm/index.jsx
@@ -15,50 +15,50 @@ const ChartShareForm = ({
 }) => {
   return (
     <Dialog
-    open={Boolean(chartToShare._id)}
-    onClose={closeDialog}
-    fullWidth={true}
-    PaperProps={{
-      component: 'form',
-      onSubmit: shareWithUsers
-    }}
-  >
-    <DialogTitle>Share Chart {chartToShare.title}?</DialogTitle>
+      open={Boolean(chartToShare._id)}
+      onClose={closeDialog}
+      fullWidth={true}
+      PaperProps={{
+        component: 'form',
+        onSubmit: shareWithUsers
+      }}
+    >
+      <DialogTitle>Share Chart {chartToShare.title}?</DialogTitle>
 
-    <DialogContent>
-      <SearchSelect
-        name="sharedWith"
-        control={shareFormControl}
-        options={sharedWithOptions}
-        formValues={shareFormValues}
-        label="Share chart"
-        actions={
-          <CardActions
-            sx={{
-              display: 'grid',
-              gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr',
-            }}
-          >
-            <Button color="primary" fullWidth onClick={clearSelectedUsers}>
-              CLEAR
-            </Button>
-            <Button
-              color="primary"
-              fullWidth
-              onClick={selectAllUsers}
-              sx={{ gridColumnStart: 4, gridColumnEnd: 6 }}
+      <DialogContent>
+        <SearchSelect
+          name="sharedWith"
+          control={shareFormControl}
+          options={sharedWithOptions}
+          formValues={shareFormValues}
+          label="Share chart"
+          actions={
+            <CardActions
+              sx={{
+                display: 'grid',
+                gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr',
+              }}
             >
-              SELECT ALL
-            </Button>
-          </CardActions>
-        }
-      />
-    </DialogContent>
-    <DialogActions>
-      <Button data-testid="cancel" onClick={closeDialog}>Cancel</Button>
-      <Button data-testid="submit" type="submit">Share</Button>
-    </DialogActions>
-  </Dialog>
+              <Button color="primary" fullWidth onClick={clearSelectedUsers}>
+                CLEAR
+              </Button>
+              <Button
+                color="primary"
+                fullWidth
+                onClick={selectAllUsers}
+                sx={{ gridColumnStart: 4, gridColumnEnd: 6 }}
+              >
+                SELECT ALL
+              </Button>
+            </CardActions>
+          }
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button data-testid="cancel" onClick={closeDialog}>Cancel</Button>
+        <Button data-testid="submit" type="submit">Share</Button>
+      </DialogActions>
+    </Dialog>
   )
 }
 
