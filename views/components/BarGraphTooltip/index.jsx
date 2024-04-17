9dbeb387aee249e8301096f872943cfa463512d0
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2024-04-17 09:30:38 -0600

    PR comments
    * fix from comments

--- views/components/BarGraphTooltip/index.jsx
+++ views/components/BarGraphTooltip/index.jsx
@@ -15,9 +15,7 @@ const BarGraphTooltip = ({
   if (!active || !payload?.length) return null
 
   const siteName = useSiteName ? label : SITE_NAMES[label] || label
-  const countsTotal = useSiteName
-    ? studyTotals[label].count
-    : studyTotals[SITE_NAMES[label] || label].count
+  const countsTotal = studyTotals[siteName].count
   const variableCounts = payload[0].payload.counts
 
   return (
