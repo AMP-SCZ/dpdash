81bf103023ef4a98d4a6a2f1fb7e4df986ced333
Jack Curtis <jack@gnar.dog>
2025-04-22 09:29:56 -0400

    move deployment-specific constants to config files

--- constants/vars.js
+++ constants/vars.js
@@ -1,15 +1,13 @@
-export const N_A = 'N/A'
-export const TOTAL_LABEL = 'Total'
-export const TOTALS = 'Totals'
-export const DISALLOWED_STUDIES = ['combined', 'files', 'ProNET']
-export const FILTER_CATEGORIES = {
-  chrcrit_part: 'Cohort',
-  included_excluded: 'Included Criteria',
-  sex_at_birth: 'Sex At Birth',
-  sites: 'Sites',
-  networks: 'Networks',
-  recruitment_status: 'Recruitment Status',
-}
+import varsConfig from '../config/vars.json'
+
+export const {
+  N_A,
+  TOTAL_LABEL,
+  TOTALS,
+  DISALLOWED_STUDIES,
+  FILTER_CATEGORIES,
+} = varsConfig
+
 export const EMAIL_REGEX = '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'
 export const NOTIFICATION_DEFAULT = {
   open: false,
