ac5795b56b3a91f35ebcfc4412fc30b7b983fdff
Jack Curtis <jack@gnar.dog>
2025-07-07 09:36:17 -0400

    hotfix log bug that is preventing startup

--- server/app.js
+++ server/app.js
@@ -90,7 +90,7 @@ const mongoURI =
   `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:27017/?tls=true&tlsCAfile=global-bundle.pem&retryWrites=false`
 
 logger.info(`
-  MongoDB URI: ${process.env.MONGODB_URI.replace(
+  MongoDB URI: ${mongoURI.replace(
     /:.*@/g,
     ':MASKED:MASKED@'
   )}
